2de2f3bbef957fdc77b86c1ad45940dc57260f7b
WalmirFerJr <walmir.jr.2004@usp.br>
2025-06-18 11:28:39 -0300

    Criando função playGame

--- script.js
+++ script.js
@@ -22,59 +22,85 @@ function getUserChoice(choice) {
     }
 }
 
-function playRound(user_choice, computer_choice) {
+function playGame() {
+
+    function playRound(computer_choice) {
+
+    let user_choice = parseInt(window.prompt("Escolha sua jogada: "))
+    user_choice = getUserChoice(user_choice)
+
+    console.log("Jogador 1 = " + user_choice)
+    console.log("Jogador 2 = " + computer_choice)
 
     switch(user_choice) {
         case "Pedra":
             if(computer_choice === "Tesoura") {
                 user_score += 1
+                console.log(`Pontos usuario = ${user_score}, Pontos computador = ${computer_score}`)
                 return "Você Venceu!"
+                
             }
             else if(computer_choice === "Pedra") {
+                console.log(`Pontos usuario = ${user_score}, Pontos computador = ${computer_score}`)
                 return "Rodada empatada"
             }
             else if(computer_choice === "Papel") {
                 computer_score += 1
+                console.log(`Pontos usuario = ${user_score}, Pontos computador = ${computer_score}`)
                 return "Você Perdeu"
             }
         
         case "Papel":
             if(computer_choice === "Tesoura") {
                 computer_score += 1
+                console.log(`Pontos usuario = ${user_score}, Pontos computador = ${computer_score}`)
                 return "Você Perdeu!"
             }
             else if(computer_choice === "Pedra") {
                 user_score += 1
+                console.log(`Pontos usuario = ${user_score}, Pontos computador = ${computer_score}`)
                 return "Você Venceu!"
             }
             else if(computer_choice === "Papel") {
+                console.log(`Pontos usuario = ${user_score}, Pontos computador = ${computer_score}`)
                 return "Rodada empatada!"
             }
 
         case "Tesoura":
             if(computer_choice === "Tesoura") {
+                console.log(`Pontos usuario = ${user_score}, Pontos computador = ${computer_score}`)
                 return "Rodada empatada!"
             }
             else if(computer_choice === "Pedra") {
                 computer_score += 1
+                console.log(`Pontos usuario = ${user_score}, Pontos computador = ${computer_score}`)
                 return "Você Perdeu!"
             }
             else if(computer_choice === "Papel") {
                 user_score += 1
+                console.log(`Pontos usuario = ${user_score}, Pontos computador = ${computer_score}`)
                 return "Você Venceu!"
             }
     }
+}
+    const rodadas = 5
 
+    for(let i = 0 ; i <= rodadas ; i++) {
+    console.log(playRound(getComputerChoice()))
+    }
 }
 
-let user_choice = parseInt(window.prompt("Escolha sua jogada: "))
-let computer_choice = getComputerChoice()
 let user_score = 0
 let computer_score = 0
 
-user_choice = getUserChoice(user_choice)
+playGame()
 
-console.log("Jogador 1 = " + user_choice)
-console.log("Jogador 2 = " + computer_choice)
-console.log(playRound(user_choice, computer_choice))
-console.log(`Pontos usuario = ${user_score}, Pontos computador = ${computer_score}`)
+if(user_score > computer_score) {
+    console.log("Parabéns, você venceu o jogo!")
+} 
+else if(user_score == computer_score) {
+    console.log("Jogo empatado!")
+}
+else {
+    console.log("Você perdeu o jogo!")
+}
