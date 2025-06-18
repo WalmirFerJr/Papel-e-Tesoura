ac817699b67c3d7e0abc333742225b5143373cd5
WalmirFerJr <walmir.jr.2004@usp.br>
2025-06-18 10:07:38 -0300

    Implementado escolha do usuario

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function getComputerChoice() {
     let escolha = Math.floor(Math.random() * 3)
+1
+    switch(escolha) {
+        case 0:
+            return "Pedra"
+        case 1:
+            return "Papel"
+        case 2:
+            return "Tesoura"
+    }2
+}
 
+function getUserChoice(escolha) {
     switch(escolha) {
         case 0:
             return "Pedra"
@@ -11,4 +22,7 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+let escolha_usuario = parseInt(window.prompt("Escolha sua jogada: "))
+
+console.log("Jogador 1 = " + getUserChoice(escolha_usuario))
+console.log("Jogador 2 = " + getComputerChoice())
